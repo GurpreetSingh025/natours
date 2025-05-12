f093d31e4a4f4c1dbdaf825bf6eb3de71ba9ad8b
gurpreet25092 <ifour.gurpreet@gmail.com>
2025-05-12 13:50:38 +0200

    used jwt to create a token when new user signed up token authentication

--- controller/authController.js
+++ controller/authController.js
@@ -1,10 +1,17 @@
+const dotenv = require('dotenv') ;
+const jwt = require('jsonwebtoken') ;
 const User = require("../model/userModel") ;
 const catchAsync = require("../utils/catchAsync")
 
+dotenv.config({path : "../config.env"}) ;
 exports.signUp = catchAsync(async (req , res, next) => {
     const newUser = await User.create(req.body) ;
+    const token = jwt.sign({id : newUser._id} , process.env.JWT_STRING , {
+        expiresIn : process.env.JWT_EXPIRE
+    })
     res.status(201).json({
          status : 'success' ,
+         token : token ,
          data : newUser
     })
 })
